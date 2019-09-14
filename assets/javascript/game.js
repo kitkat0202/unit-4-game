4a57016a8db117ec44f2d31aa1f8bfbe34e8be4a
KatHe <he.katherine321@gmail.com>
2019-09-14 02:53:04 -0400

    made HP and ATK more accurate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 $(function() {
-    let log = console.log
+    const log = console.log
     let clickOn = false
     let clickStart = false
-    var toggler = true
+    let toggler = true
     let savedChara = []
     let playChara = []
 
@@ -10,14 +10,17 @@ $(function() {
     let lose = 0
     let defeted = 0
 
+    let playerName = ""
     let playerOrgHealth = 0
     let playerHealth = 0
     let playerAttack = 0
-    let playerOrgAttack = 0
+    let playerMove = ""
     let playerIsChosen = false
 
+    let computerName = ""
     let computerOrgHealth = 0
     let computerHealth = 0
+    let computerMove = ""
     let computerAttack = 0
     let computerIsChosen = false
 
@@ -26,60 +29,71 @@ $(function() {
         return Math.floor(Math.random() * (max - min + 1))+ min
     }
 
-    function CharaCreater(n, imgf, imgb, h, a) {
-        this.name = n
-        this.imgFront = imgf
-        this.imgBack = imgb
-        this.health = h
-        this.attack = a
-    }
-
-
     let getPokemonById = (charaNum) => {
-        log("running")                                      //////////// remove
         let id = randomNumber(1, 150)
         let url = `https://pokeapi.co/api/v2/pokemon/${id}/`
 
         fetch(url)
-        .then(responce => responce.json())
-        .then(data => {
-           return data.name.toUpperCase()
-        }).then(dataName => {
-            log(dataName)                               ///////////////// remove
-            let front = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-            let back = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
-            let hp
-            let atk
+            .then(res => res.json())
+            .then(data => {
+                // Move info
+                let moveLength = data.moves.length - 1
+                let moveNumRandom = randomNumber(0, moveLength)
+                let moveURL = data.moves[moveNumRandom].move.url
 
-            if ((id > 143 || id === 3 || id === 6 || id === 9 || id === 130) && id < 152) {
-                hp = randomNumber(150, 200)
-                atk = randomNumber(15, 25)
-            } else if (id === 129 ) {
-                hp = randomNumber(50, 90)
-                atk = 1
-            } else {
-                hp = randomNumber(100, 150)
-                atk = randomNumber(10, 20)
-            }
+                // Important Pokemon Data Extracted
+                let thisPokemon = {
+                    name: data.name.toUpperCase(),
+                    imgFront:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+                    imgBack:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`,
+                    // health: Math.floor((data.base_experience * 2 + randomNumber(1,30) + (randomNumber(1,255)/4)) * (data.stats[5].base_stat/100 + 10)),
+                    health: Math.floor((data.stats[5].base_stat * 2 + randomNumber(1,30) + (randomNumber(1,255)/4)) * (10/100 + 10)),
+                    move: data.moves[moveNumRandom].move.name,
+                    attack: 5
+                }
 
-            construct = new CharaCreater(dataName, front, back, hp, atk)
-            return construct
-        }).then(thechara => {
-            charaNum.push(thechara)
-            
-        }).catch(error => {
-            log(`the error is ${error}`)
-            $(".video, .gameplay").addClass("disappear")
-            $(".nintendo").removeClass("disappear")
-            $(".nintendo").removeClass("dark")
-            $(".nintendo").addClass("light")
-            $(".nintendo").html($("<p>").text("Sorry Server Down..."))
+                while (badMoves.indexOf(thisPokemon.move) !== -1) {
+                    moveNumRandom = randomNumber(0, moveLength)
+                    moveURL = data.moves[moveNumRandom].move.url
+                    thisPokemon.move = data.moves[moveNumRandom].move.name
+                }
 
-            setTimeout(() => {
-                clearAll()
-            }, 2500)
-            
-        })
+                // Fetch the MOVE's correct ATK power
+                // Check if ATK and HP are a numbers
+                return (fetch(moveURL)
+                    .then(res => res.json())
+                    .then(data => {
+                        // If HP is not a number, make HP 1000
+                        if (typeof thisPokemon.health !== "number") {
+                            thisPokemon.health = 1000
+                        }
+
+                        // If ATK is a number replace ATK with correct num
+                        if (typeof data.power === "number") {
+                            thisPokemon.attack = data.power
+                        }
+                        return thisPokemon
+                    }) )
+
+                
+            })
+            .then(thechara => {
+                thechara.move = thechara.move.toUpperCase()
+                charaNum.push(thechara)
+                
+            })
+            .catch(error => {
+                log(`the error is ${error}`)
+                $(".video, .gameplay").addClass("disappear")
+                $(".nintendo").removeClass("disappear")
+                $(".nintendo").removeClass("dark")
+                $(".nintendo").addClass("light")
+                $(".nintendo").html($("<p>").text("Sorry Server Down..."))
+
+                setTimeout(() => {
+                    clearAll()
+                }, 2500)
+            })
 
     }
 
@@ -103,19 +117,24 @@ $(function() {
         playChara = []
 
         defeted = 0
-    
+
+        playerName = ""
         playerOrgHealth = 0
         playerHealth = 0
+        playerMove = ""
         playerAttack = 0
         playerOrgAttack = 0
         playerIsChosen = false
+        computerName = ""
         computerOrgHealth = 0
         computerHealth = 0
+        computerMove = ""
         computerAttack = 0
         computerIsChosen = false
 
     }
 
+    // Lagg in getting API so in initial grab will get multi characters to save for later
     let addToCharaPlay = () => {
         for (let i = 0; i < 4; i++) {
             if (playChara.length < 4) {
@@ -133,6 +152,7 @@ $(function() {
             $(`#chara-${i}`).append($("<img>").attr("src", playChara[i].imgFront))
             $(`#chara-${i}`).append($("<p>").html(playChara[i].name).addClass("chara-name"))
             $(`#chara-${i}`).append($("<p>").html(`Health: ${playChara[i].health}`).addClass("chara-health"))
+            $(`#chara-${i}`).append($("<p>").html(`Move: ${playChara[i].move}`).addClass("chara-move"))
             $(`#chara-${i}`).append($("<p>").html(`Attack: ${playChara[i].attack}`).addClass("chara-attack"))
         }
 
@@ -189,24 +209,6 @@ $(function() {
 
     }
 
-    let updateOutcome = () => {
-        //lower comp health by player health
-        computerHealth -= playerAttack
-        $(".right-status .status-health").html(`HP: ${computerHealth}`)
-        healthBarDisplay(computerOrgHealth, computerHealth, ".right-status .health-bar")
-
-        //lower player health by comp health
-        playerHealth -= computerAttack
-        $(".left-status .status-health").html(`HP: ${playerHealth}`)
-        healthBarDisplay(playerOrgHealth, playerHealth, ".left-status .health-bar")
-        playerAttack += playerOrgAttack
-        $(".left-status .status-attack").html(`ATK: ${playerAttack}`)
-
-        $(".info-center").empty()
-        $(".info-center").append($("<p>").text(`Player attacks with ${playerAttack}ATK. Computer Health is now ${computerHealth} HP`))
-        $(".info-center").append($("<p>").text(`Computer attacks with ${computerAttack}ATK. Player Health is now ${playerHealth} HP`))
-    }
-
     let checkWinLose = () => {
         if (computerHealth <= 0) {
             $(".info-center").empty()
@@ -222,7 +224,7 @@ $(function() {
                     divDisplay()
                     $(".nintendo").addClass("disappear")
                     $(".choosing").removeClass("disappear")
-                }, 1500);
+                }, 500);
             } else {
                 computerIsChosen = false
                 $(".right-battle, .right-status").empty()
@@ -234,7 +236,6 @@ $(function() {
             $(".info-center").empty()
             clickOn = false
             lose++
-            //NEED SOME LOSE SCREEN
             $(".info-center").append($("<p>").text(`You Lose...`))
             setTimeout(() => {
                 clearAll()
@@ -242,10 +243,29 @@ $(function() {
                 divDisplay()
                 $(".nintendo").addClass("disappear")
                 $(".choosing").removeClass("disappear")
-            }, 1500);
+            }, 500);
         }
+    }
 
+    let updateOutcome = () => {
+        //lower comp health by player health
+        computerHealth -= playerAttack
+        $(".right-status .status-health").html(`HP: ${computerHealth}`)
+        healthBarDisplay(computerOrgHealth, computerHealth, ".right-status .health-bar")
 
+        //lower player health by comp health
+        playerHealth -= computerAttack
+        $(".left-status .status-health").html(`HP: ${playerHealth}`)
+        healthBarDisplay(playerOrgHealth, playerHealth, ".left-status .health-bar")
+        playerAttack += 10
+        $(".left-status .status-attack").html(`ATK: ${playerAttack}`)
+
+        $(".info-center").empty()
+        $(".info-center").append($("<p>").text(`${playerName} attacks with ${playerMove} ${playerAttack} ATK`))
+        $(".info-center").append($("<p>").addClass("bot").text(`${computerName} Health is now ${computerHealth} HP`))
+        $(".info-center").append($("<p>").text(`${computerName} attacks with ${computerMove} ${computerAttack} ATK`))
+        $(".info-center").append($("<p>").addClass("bot").text(`${playerName} Health is now ${playerHealth} HP`))
+        checkWinLose()
     }
 
 
@@ -262,13 +282,15 @@ $(function() {
         if (computerIsChosen) {
             $(".choosing").addClass("disappear")
             $(".gameplay").removeClass("disappear")
-            return false
+            // return false
         }
 
         if (playerIsChosen) {
             //add computer in field
+            computerName = choice.name
             computerOrgHealth = choice.health
             computerHealth = choice.health
+            computerMove = choice.move
             computerAttack = choice.attack
             $(".right-battle").append($("<img>").attr("src", choice.imgFront))
             statusDisplay(".right-status")
@@ -288,8 +310,10 @@ $(function() {
             
         } else {
             //add player in field
+            playerName = choice.name
             playerOrgHealth = choice.health
             playerHealth = choice.health
+            playerMove = choice.move
             playerOrgAttack = choice.attack
             playerAttack = choice.attack
             $(".left-battle").append($("<img>").attr("src", choice.imgBack))
@@ -324,7 +348,6 @@ $(function() {
     $(".atk").on("click", function() {
         if (clickOn) {
             updateOutcome()
-            checkWinLose()
         }
     })
 
